1c549e0c8911fec556fd098c2640289941e52b54
Sayali Patil <spsayalipatil2003@gmail.com>
2026-02-24 13:04:44 +0530

    UserSchema is created

---
+++ server.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const app=require('./src/app');
+const connectDB=require('./src/config/db');
+connectDB();
+app.listen(3000,(req,res)=>{
+    console.log("server is running on port 3000")
+})
---
+++ src/models/user.model.js
@@ -0,0 +1,40 @@
+const mongoose=require('mongoose');
+const bcrypt=require('bcryptjs');
+const userSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        reqired:[true,"email is required for registration"],
+        trim:true,
+        lowercase:true,
+        match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please add a valid email address.'
+    ],
+    unique:[true,"email already exist."]
+    },
+    name:{
+        type:String,
+        required:[true,"Name is required for registration"],
+    },
+    password:{
+        type:String,
+        required:[true,"Password is required for registration"],
+        minLength:[6,"Password must be at least 6 characters long"],
+        select:false
+    },
+    timestamps:true
+});
+userSchema.pre('save',async function(next){
+
+    if(!this.isModified('password')){
+        return next();
+    }
+    const hash=await bcrypt.hash(this.password,10)
+    this.password=hash;
+    return next();//password convert in hash and save in database
+})
+userSchema.methods.comparePassword=async function(password){
+    return await bcrypt.compare(password,this.password);
+}
+const userModel=mongoose.model('User',userSchema);
+module.exports=userModel;
